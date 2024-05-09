c9129029e5d6c541f1fca5a45eb44ee8d524ae13
Daniel Noland <daniel.noland@dnoland.cloud>
2024-05-08 19:34:52 -0600

    Add support for `tc-actions`

--- src/traffic_control/handle.rs
+++ src/traffic_control/handle.rs
@@ -8,7 +8,10 @@ use super::{
     TrafficClassGetRequest, TrafficFilterDelRequest, TrafficFilterGetRequest,
     TrafficFilterNewRequest,
 };
-use crate::Handle;
+use crate::{
+    Handle, TrafficActionDelRequest, TrafficActionGetRequest,
+    TrafficActionNewRequest,
+};
 
 #[derive(Debug, Clone)]
 pub struct QDiscHandle(Handle);
@@ -143,5 +146,48 @@ impl TrafficChainHandle {
     /// `tc chain show dev <iface_name>`)
     pub fn get(&mut self) -> TrafficChainGetRequest {
         TrafficChainGetRequest::new(self.handle.clone(), self.ifindex)
+    }
+}
+
+#[non_exhaustive]
+pub struct TrafficActionHandle {
+    handle: Handle,
+}
+
+impl TrafficActionHandle {
+    /// Create a new traffic action manipulation handle.
+    pub fn new(handle: Handle) -> Self {
+        TrafficActionHandle { handle }
+    }
+
+    /// Retrieve the list of actions.
+    ///
+    /// Equivalent to
+    ///
+    /// ```bash
+    /// tc action list actions $act_type
+    /// ```
+    pub fn get(&mut self) -> TrafficActionGetRequest {
+        TrafficActionGetRequest::new(self.handle.clone())
+    }
+
+    /// Add a new action.
+    ///
+    /// Equivalent to
+    /// ```bash
+    /// tc action add action $act_type $act_options index $index
+    /// ```    
+    pub fn add(&mut self) -> TrafficActionNewRequest {
+        TrafficActionNewRequest::new(self.handle.clone())
+    }
+
+    /// Delete an action.
+    ///
+    /// Equivalent to
+    /// ```bash
+    /// tc action del action $act_type index $index
+    /// ```
+    pub fn del(&mut self) -> TrafficActionDelRequest {
+        TrafficActionDelRequest::new(self.handle.clone())
     }
 }
--- src/traffic_control/mod.rs
+++ src/traffic_control/mod.rs
@@ -1,14 +1,36 @@
 // SPDX-License-Identifier: MIT
 
+//! Traffic control manipulation utilities.
+//! See [`tc`].
+//!
+//! [`tc`]: https://man7.org/linux/man-pages/man8/tc.8.html
+
+mod add_action;
 mod add_filter;
 mod add_qdisc;
+mod del_action;
 mod del_filter;
 mod del_qdisc;
 mod get;
 mod handle;
+
 #[cfg(test)]
 mod test;
 
+pub use self::add_action::TrafficActionNewRequest;
+pub use self::add_filter::TrafficFilterNewRequest;
+pub use self::add_qdisc::QDiscNewRequest;
+pub use self::del_action::TrafficActionDelRequest;
+pub use self::del_filter::TrafficFilterDelRequest;
+pub use self::del_qdisc::QDiscDelRequest;
+pub use self::get::{
+    QDiscGetRequest, TrafficActionGetRequest, TrafficActionKind,
+    TrafficChainGetRequest, TrafficClassGetRequest, TrafficFilterGetRequest,
+};
+pub use self::handle::{
+    QDiscHandle, TrafficActionHandle, TrafficChainHandle, TrafficClassHandle,
+    TrafficFilterHandle,
+};
 pub use self::{
     add_filter::TrafficFilterNewRequest,
     add_qdisc::QDiscNewRequest,
